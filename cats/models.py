03744ea5bdb49e40b8a5a2913592b7221b2e7f30
Konstantin Bekurin <kbekurin@bk.ru>
2023-02-16 16:30:09 +0300

    Final version

--- cats/models.py
+++ cats/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
+
 class Achievement(models.Model):
     name = models.CharField(max_length=64)
 
@@ -18,12 +27,14 @@ class Owner(models.Model):
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
+    color = models.CharField(max_length=16, choices=CHOICES)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
         Owner,
         related_name='cats',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        blank=True, 
+        null=True
     )
     # Связь будет описана через вспомогательную модель AchievementCat
     achievements = models.ManyToManyField(Achievement, through='AchievementCat')
--- cats/serializers.py
+++ cats/serializers.py
@@ -1,21 +1,26 @@
+import datetime as dt
 from rest_framework import serializers
 
-from .models import Achievement, AchievementCat, Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -37,6 +42,17 @@ class CatSerializer(serializers.ModelSerializer):
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
+    
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
